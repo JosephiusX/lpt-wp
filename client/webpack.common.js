bee8429ca02792a4de70efbcd157ab4544203794
josephius x <josephiusx@gmail.com>
2023-05-22 21:45:15 -0700

    added babel along with its loaders aanbd presets. configured in webpack.common.js

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -13,6 +13,16 @@ module.exports = {
     module:{
         rules:[
             {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-react'],
+                    },
+                },
+              },
+            {
                 test:/\.txt$/i,
                 type:'asset/source', // raw-loader
             },
