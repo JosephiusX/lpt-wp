ed527510e21809179338bf893f727bc83aa554b8
josephius x <josephiusx@gmail.com>
2023-05-22 19:16:13 -0700

    changed entry point from index to main in webpack.common.js

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     context:path.resolve(__dirname,'src'),
-    entry:'./page.js',
+    entry:'./main.js',
     output:{
         clean:true,
         filename:'[name].ini.js',
