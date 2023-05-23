e4aa778f1af6c20eb71bb8036d35b97399ac3e1e
josephius x <josephiusx@gmail.com>
2023-05-22 21:55:06 -0700

    added public dist and index.html

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     context:path.resolve(__dirname,'src'),
-    entry:'./main.js',
+    entry:'./main.jsx',
     output:{
         clean:true,
         filename:'[name].ini.js',
