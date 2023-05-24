fa93c92971a968ea512f3839bdd3723aba533668
josephius x <josephiusx@gmail.com>
2023-05-24 12:17:30 -0700

    A BUTTON APPEARED

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
@@ -18,12 +18,12 @@ class App extends Component {
       return (
       <div className="container">
         <BrowserRouter>
-          <div>
             <Navigation className="box" />
+          <Routes>
             <Route path="/" exact component={Landing} />
             <Route path="/prices" exact component={Prices} />
             <Route path="/surveys/new" component={SurveyNew}/>
-          </div>
+          </Routes>
         </BrowserRouter>
       </div>
     );
