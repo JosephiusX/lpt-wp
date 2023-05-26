b76cb8302f65eab9357e32f6fffa9912a5808a39
josephius x <josephiusx@gmail.com>
2023-05-25 18:13:05 -0700

    fixed className errors

--- client/src/components/BreakdownCards.jsx
+++ client/src/components/BreakdownCards.jsx
@@ -4,24 +4,24 @@ import React from "react";
 
 const BreakdownCards = () => {
     return (
-        <section class="section-price-breakdown" id="section-price-breakdown">
-        <div class="u-center-text u-margin-bottom-big">
-            <h2 class="heading-secondary">
+        <section className="section-price-breakdown" id="section-price-breakdown">
+        <div className="u-center-text u-margin-bottom-big">
+            <h2 className="heading-secondary">
                 Price Breakdown
             </h2>
         </div>
     
-        <div class="row">
-            <div class="col-1-of-2">
-               <div class="breakdown-card">
-                   <div class="breakdown-card__side breakdown-card__side--front">
-                        <div class="breakdown-card__picture breakdown-card__picture--1">
+        <div className="row">
+            <div className="col-1-of-2">
+               <div className="breakdown-card">
+                   <div className="breakdown-card__side breakdown-card__side--front">
+                        <div className="breakdown-card__picture breakdown-card__picture--1">
                             &nbsp;
                         </div>
-                        <h4 class="breakdown-card__heading">
-                            <span class="breakdown-card__heading-span breakdown-card__heading-span--1">Package 1</span>
+                        <h4 className="breakdown-card__heading">
+                            <span className="breakdown-card__heading-span breakdown-card__heading-span--1">Package 1</span>
                         </h4>
-                        <div class="breakdown-card__details">
+                        <div className="breakdown-card__details">
                             <ul>
                                 <li>  1-25</li>
                                 <li> 3 Pizza menu (base) </li>
@@ -31,29 +31,29 @@ const BreakdownCards = () => {
                             </ul>
                         </div>
                    </div>
-                   <div class="breakdown-card__side breakdown-card__side--back breakdown-card__side--back-1">
-                        <div class="breakdown-card__cta">
-                            <div class="breakdown-card__price-box">
-                                <p class="breakdown-card__about-me"></p>
-                                <p class="breakdown-card__price-value u-center-text">$1800.00</p>
+                   <div className="breakdown-card__side breakdown-card__side--back breakdown-card__side--back-1">
+                        <div className="breakdown-card__cta">
+                            <div className="breakdown-card__price-box">
+                                <p className="breakdown-card__about-me"></p>
+                                <p className="breakdown-card__price-value u-center-text">$1800.00</p>
                             </div>
-                            <a href="#popup" class="btn btn--white">Contact Us</a>
+                            <a href="#popup" className="btn btn--white">Contact Us</a>
                         </div>
                     </div>
                </div>
             </div>
     
     
-            <div class="col-1-of-2">
-                <div class="breakdown-card">
-                    <div class="breakdown-card__side breakdown-card__side--front">
-                        <div class="breakdown-card__picture breakdown-card__picture--2">
+            <div className="col-1-of-2">
+                <div className="breakdown-card">
+                    <div className="breakdown-card__side breakdown-card__side--front">
+                        <div className="breakdown-card__picture breakdown-card__picture--2">
                             &nbsp;
                         </div>
-                        <h4 class="breakdown-card__heading">
-                            <span class="breakdown-card__heading-span breakdown-card__heading-span--2">Package 2 </span>
+                        <h4 className="breakdown-card__heading">
+                            <span className="breakdown-card__heading-span breakdown-card__heading-span--2">Package 2 </span>
                         </h4>
-                        <div class="breakdown-card__details">
+                        <div className="breakdown-card__details">
                             <ul>
                                 <li>  1-25</li>
                                 <li> 3 Pizza menu (base) </li>
@@ -64,13 +64,13 @@ const BreakdownCards = () => {
                         </div>
     
                     </div>
-                    <div class="breakdown-card__side breakdown-card__side--back breakdown-card__side--back-2">
-                        <div class="breakdown-card__cta">
-                            <div class="breakdown-card__price-box">
-                            <p class="breakdown-card__price-value u-center-text">$1800.00</p>
-                                {/* <p class="breakdown-card__price-value">$497</p> */}
+                    <div className="breakdown-card__side breakdown-card__side--back breakdown-card__side--back-2">
+                        <div className="breakdown-card__cta">
+                            <div className="breakdown-card__price-box">
+                            <p className="breakdown-card__price-value u-center-text">$1800.00</p>
+                                {/* <p className="breakdown-card__price-value">$497</p> */}
                             </div>
-                            <a href="#popup" class="btn btn--white">Contact Us</a>
+                            <a href="#popup" className="btn btn--white">Contact Us</a>
                         </div>
                     </div>
                 </div>
@@ -78,8 +78,8 @@ const BreakdownCards = () => {
         </div>
     
 
-        {/* <div class="u-center-text u-margin-top-huge">
-            <a href="#" class="btn btn--green">Contact Us</a>
+        {/* <div className="u-center-text u-margin-top-huge">
+            <a href="#" className="btn btn--green">Contact Us</a>
         </div> */}
     </section>
       );
