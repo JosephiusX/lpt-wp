ba5786f23eddf49da886796255e8923af0beab6b
josephius x <josephiusx@gmail.com>
2023-05-31 21:55:08 -0700

    dev/prod paartially working

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// C:\Users\josep\OneDrive\Desktop\lpt-cra\server\index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
@@ -18,7 +19,7 @@ app.use(
     name: 'session',
     keys: [keys.cookieKey],
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    // secure: process.env.NODE_ENV === 'production' // true if production
+    secure: process.env.NODE_ENV === 'production' // true if production
   })
 );
 
